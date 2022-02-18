5e16cbc68e81ff9e313c1a595868a9578e0e4eea
Christopher Harrison <chrhar@microsoft.com>
2022-02-18 23:23:25 +0000

    Removed extra APIs

--- api/server.js
+++ api/server.js
@@ -7,33 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-app.get('/api/dogs', async (req, res) => {
-    try {
-        await database.connect();
-        res.json(await database.get());
-    } finally {
-        database.close();
-    }
-});
-
-app.get('/api/dogs/:id', async (req, res) => {
-    try {
-        database.connect();
-        res.json(await database.getOne(req.params['id']));
-    } finally {
-        database.close();
-    }
-})
-
-app.post('/api/dogs', async (req, res) => {
-    try {
-        database.connect();
-        res.json(await database.create(req.body));
-    } finally {
-        database.close();
-    }
-})
-
 app.get('/api/breeds', async (req, res) => {
     try {
         database.connect();
